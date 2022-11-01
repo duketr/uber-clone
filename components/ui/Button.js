032b7a03ecc007839bd8fdc2d85346d66c1b718d
Tran Minh Duc <tranminhduc259@gmail.com>
2022-11-01 21:24:31 +0700

    Update Button.js

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,11 +1,14 @@
-import { View, Pressable, Text, StyleSheet } from "react-native";
+import { View, Pressable, Text, StyleSheet, Dimensions } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function Button({children, onPress}) {
     return (
         <View>
-            <Pressable onPress={onPress}>
+            <Pressable
+            style={({pressed}) => pressed ? [styles.buttonContainer, styles.pressed] : styles.buttonContainer}
+            onPress={onPress}>
                 <View>
-                    <Text>{children}</Text>
+                    <Text style={styles.buttonText}>{children}</Text>
                 </View>
             </Pressable>
         </View>
@@ -13,12 +16,19 @@ function Button({children, onPress}) {
 }
 export default Button;
 const styles = StyleSheet.create({
-    button: {
+    buttonContainer: {
         borderRadius: 5,
-        padding: 10,
+        paddingVertical: 10,
+        minWidth: (Dimensions.get('window').width)/1.3,
+        borderWidth: 1,
+        backgroundColor: GlobalStyles.colors.primary500,
     },
     buttonText: {
         color: 'white',
-        textAlign: 'center'
+        textAlign: 'center',
+        fontSize: 16
+    },
+    pressed: {
+        opacity: 0.75
     }
 })
