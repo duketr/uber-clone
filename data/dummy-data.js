b2f839a800523bee76efc6437e03178347dcfb21
Tran Minh Duc <tranminhduc259@gmail.com>
2022-11-01 21:25:41 +0700

    Update dummy-data.js

---
+++ data/dummy-data.js
@@ -0,0 +1,50 @@
+export const TRIP_HISTORY = [
+    {
+        id: 1,
+        date: new Date('2022-02-20'),
+        type: 'Economy',
+        cost: 29.99,
+        startDestination: '12 A Street',
+        endDestination: '24 B Street'
+    },
+    {
+        id: 2,
+        date: new Date('2022-10-30'),
+        type: 'Normal',
+        cost: 19.99,
+        startDestination: '23 C Street',
+        endDestination: '46 D Street'
+    },
+    {
+        id: 3,
+        date: new Date('2022-09-15'),
+        type: 'Luxury',
+        cost: 49.99,
+        startDestination: '37 E Street',
+        endDestination: '74 F Street'
+    },
+    {
+        id: 4,
+        date: new Date('2022-05-13'),
+        type: 'Normal',
+        cost: 9.49,
+        startDestination: '88 G Street',
+        endDestination: '176 H Street'
+    },
+    {
+        id: 5,
+        date: new Date('2022-10-15'),
+        type: 'Economy',
+        cost: 10.29,
+        startDestination: '11 I Street',
+        endDestination: '22 J Street'
+    },
+    {
+        id: 6,
+        date: new Date('2022-02-20'),
+        type: 'Luxury',
+        cost: 99.89,
+        startDestination: '60 K Street',
+        endDestination: '120 L Street'
+    },
+]
