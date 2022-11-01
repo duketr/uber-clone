8a658c07b2ad62c58f2f37af0fba663f8cfa6d38
Tran Minh Duc <tranminhduc259@gmail.com>
2022-11-01 21:20:34 +0700

    Update SignupScreen.js

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,15 +1,60 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions, TextInput } from "react-native";
+import Button from "../components/ui/Button";
 
-function SignupScreen() {
+function LoginScreen({navigation}) {
     return (
-    <View>
-        <Text>This is the Sign Up Screen</Text>
+    <View style={styles.container}>
+        <View style={styles.inputContainers}>
+            <TextInput 
+             textAlign="center"
+             placeholder="Email"
+            />
+        </View>
+        <View style={styles.inputContainers}>
+            <TextInput 
+             textAlign="center"
+             placeholder="Phone Number"
+            />
+        </View>
+        <View style={styles.inputContainers}>
+            <TextInput 
+             textAlign="center"
+             placeholder="Password"
+            />
+        </View>
+        <View style={styles.inputContainers}>
+            <TextInput 
+             textAlign="center"
+             placeholder="Re-enter Password"
+            />
+        </View>
+        <View style={styles.buttonContainers}>
+            <Button onPress={() => navigation.navigate('NavDrawer')}>Sign Up</Button>
+        </View>
+        <View style={styles.buttonContainers}>
+            <Button onPress={() => navigation.navigate('Login')}>Already have an account? Log In</Button>
+        </View>
     </View>
     );
 }
 
-export default SignupScreen;
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+    },
+    buttonContainers: {
+        margin: 10
+    },
+    inputContainers: {
+        borderWidth: 2,
+        borderColor: '#aaa',
+        minWidth: Dimensions.get('window').width/1.5,
+        minHeight: Dimensions.get('window').height/30,
+        marginVertical: 5
+    } 
 })
